6f93d5589424b7e971330a3b2504959f468e2c3f
Kamalendu Garai <kamalendugarai@gmail.com>
2021-04-04 17:59:57 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# webp-avif-browsers-support-detection
+
+This is a utility to check if the browser does support for webp and avif next generation image types.
+
+Next generation image types are very good in compression. So, you should start working with those
+
+## Usage
+
+```sh
+$ npm i webp-avif-browsers-support-detection --save-dev
+```
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const detect = function (im = []) {
+  var images = [
+    {
+      type: "webp",
+      url:
+        "data:image/webp;base64,UklGRhwAAABXRUJQVlA4TBAAAAAvAAAAEAfQpv5HmQMR0f8A",
+    },
+    {
+      type: "avif",
+      url:
+        "data:image/avif;base64,AAAAHGZ0eXBtaWYxAAAAAG1pZjFhdmlmbWlhZgAAAPFtZXRhAAAAAAAAACFoZGxyAAAAAAAAAABwaWN0AAAAAAAAAAAAAAAAAAAAAA5waXRtAAAAAAABAAAAHmlsb2MAAAAABEAAAQABAAAAAAEVAAEAAAAeAAAAKGlpbmYAAAAAAAEAAAAaaW5mZQIAAAAAAQAAYXYwMUltYWdlAAAAAHBpcHJwAAAAUWlwY28AAAAUaXNwZQAAAAAAAAABAAAAAQAAABBwYXNwAAAAAQAAAAEAAAAVYXYxQ4EgAAAKBzgABpAQ0AIAAAAQcGl4aQAAAAADCAgIAAAAF2lwbWEAAAAAAAAAAQABBAECg4QAAAAmbWRhdAoHOAAGkBDQAjITFkAAAEgAAAB5TNw9UxdXU6F6oA == ",
+    },
+  ].concat(im);
+  var $html = document.getElementsByTagName("html")[0];
+  var defineImage = function (obj) {
+    let img = new Image();
+    img.src = obj.url;
+    img.onerror = function (err) {
+      var classes = new Set(
+        !!$html.getAttribute("class")
+          ? $html.getAttribute("class").split(" ")
+          : []
+      );
+      classes.add("no-" + obj.type.toLowerCase());
+      $html.setAttribute("class", [...classes].join(" "));
+      console.log(
+        obj.type.toUpperCase() + " not supported in this browser",
+        err
+      );
+    };
+    img.onload = function (inst) {
+      var classes = new Set(
+        !!$html.getAttribute("class")
+          ? $html.getAttribute("class").split(" ")
+          : []
+      );
+      classes.add(obj.type.toLowerCase());
+      $html.setAttribute("class", [...classes].join(" "));
+      console.log(obj.type.toUpperCase() + " supported in this browser");
+    };
+  };
+  images.map((elm, i) => {
+    defineImage(elm);
+  });
+};
+export default detect;
